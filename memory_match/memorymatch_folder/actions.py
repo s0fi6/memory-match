f7cd174d354ccbd052a6285a104d937eb7c4ba72
s0fi6 <sofigp2507@gmail.com>
2024-04-08 13:44:11 -0400

    Actions and Instructions

---
+++ memory_match/memorymatch_folder/actions.py
@@ -0,0 +1,16 @@
+#Reveal cards 
+def reveal_cards(board, row, column):
+    pass
+
+#Hide cards
+def hide_cards(board, row, column):
+    pass
+
+#Check matching cards
+def match(board, row1, column1, row2, column2):
+    pass
+
+#Game over
+def game_over(board):
+    pass
+
