e1976cda8e5034707ea9ebcdb016a777ec31d887
s0fi6 <sofigp2507@gmail.com>
2024-04-08 20:36:59 -0400

    New things (actions, board)

--- memory_match/memorymatch_folder/actions.py
+++ memory_match/memorymatch_folder/actions.py
@@ -1,16 +1,22 @@
+#Suffle cards
+def shuffle_cards(board, cards, row, column):
+    pass
+
 #Reveal cards 
 def reveal_cards(board, row, column):
     pass
 
-#Hide cards
-def hide_cards(board, row, column):
+#Flipp cards
+def flipp_cards(board, row, column):
+    pass
+
+#Pick card
+def pick_card(board, reveal_cards):
     pass
 
 #Check matching cards
-def match(board, row1, column1, row2, column2):
+def match(board, row1, column1, row2, column2, row3, column3):
     pass
 
-#Game over
-def game_over(board):
-    pass
+
 
---
+++ memory_match/memorymatch_folder/board.py
@@ -0,0 +1,19 @@
+#Create new board
+def new_board(rows, columns):
+    pass
+
+#Print current board
+def print_board(row1, column1, row2, column2, row3, column3):
+    pass
+
+#All cards ready
+def all_cards(rows, columns):
+    pass
+
+#Winner
+def game_winner(board, word_winner):
+    pass
+
+#Game over
+def game_over(board, word_loser):
+    pass
