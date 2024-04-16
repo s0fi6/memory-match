216a52ffde2b2fc6510d1a56d9b5dc8170f65441
BernardoTorano <a01710456@tec.mx>
2024-04-16 09:12:41 -0400

    2d board

--- memory_match/memorymatch_folder/board.py
+++ memory_match/memorymatch_folder/board.py
@@ -1,10 +1,24 @@
 #Create new board
 def new_board(rows, columns):
-    print("Create new board")
+   board = [[" ", " ", " "],
+            [" ", " ", " "],
+            [" ", " ", " "],
+            [" ", " ", " "]]
+   return board
 
 #Print current board
 def print_board(row1, column1, row2, column2, row3, column3):
-    print("Print current board")
+    print(
+        f"""
+ {board[0][0]} |  {board[0][1]} |  {board[0][2]} |  {board[0][3]} 
+ -----------
+ {board[1][0]} |  {board[1][1]} |  {board[1][2]} |  {board[1][3]} 
+ -----------
+ {board[2][0]} |  {board[2][1]} |  {board[2][2]} |  {board[2][3]} 
+ -----------
+ {board[3][0]} |  {board[3][1]} |  {board[3][2]} |  {board[3][3]} 
+ """
+     )
 
 #All cards ready
 def all_cards(rows, columns):
