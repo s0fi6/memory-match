8e7450cb26ae503d608aacf3f2bfc92e059b64de
Jerome Simon <jerome.simon@hotmail.com>
2024-04-30 17:31:44 -0400

    Consider "flipped" when printing

--- memory_match/board.py
+++ memory_match/board.py
@@ -11,18 +11,18 @@ def new_board():
     return board, flipped
 
 #Print current board
-def print_board(board):
+def print_board(board, flipped):
     print(
         f"""
- {board[0][0]} |  {board[0][1]} |  {board[0][2]} |  {board[0][3]} 
+ {board[0][0] if flipped[0][0] else "****"} |  {board[0][1] if flipped[0][1] else "****"} |  {board[0][2] if flipped[0][2] else "****"} |  {board[0][3] if flipped[0][3] else "****"} 
  ------------------
- {board[1][0]} |  {board[1][1]} |  {board[1][2]} |  {board[1][3]} 
+ {board[1][0] if flipped[1][0] else "****"} |  {board[1][1] if flipped[1][1] else "****"} |  {board[1][2] if flipped[1][2] else "****"} |  {board[1][3] if flipped[1][3] else "****"} 
  ------------------
- {board[2][0]} |  {board[2][1]} |  {board[2][2]} |  {board[2][3]} 
+ {board[2][0] if flipped[2][0] else "****"} |  {board[2][1] if flipped[2][1] else "****"} |  {board[2][2] if flipped[2][2] else "****"} |  {board[2][3] if flipped[2][3] else "****"} 
  ------------------
- {board[3][0]} |  {board[3][1]} |  {board[3][2]} |  {board[3][3]} 
+ {board[3][0] if flipped[3][0] else "****"} |  {board[3][1] if flipped[3][1] else "****"} |  {board[3][2] if flipped[3][2] else "****"} |  {board[3][3] if flipped[3][3] else "****"} 
  """
-     )
+    )
 
 #All cards ready
 def all_cards(rows, columns):
