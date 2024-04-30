651b761cd34dc1d237358ea92df6049a67140ed8
MichB1223 <165333457+MichB1223@users.noreply.github.com>
2024-04-30 09:09:48 -0400

    p

--- memory_match/memorymatch_folder/actions.py
+++ memory_match/memorymatch_folder/actions.py
@@ -1,6 +1,16 @@
 #Shuffle cards
-def shuffle_cards(board, cards):
-    print("shuffle cards")
+import random
+
+def shuffle_cards(board):
+    for sublist in board:
+        random.shuffle(sublist)
+
+board = [["DOG", "CAT", "GIRAFFE", "RACOON"],
+    ["DOG", "CAT", "GIRAFFE", "RACOON"],
+    ["BEE", "PANDA", "FISH", "MONKEY"],
+    ["BEE", "PANDA", "FISH", "MONKEY"]]
+shuffle_cards(board)
+print(board)
 
 #Reveal cards 
 def reveal_cards(board, row, column):
@@ -15,7 +25,7 @@ def pick_card(board, reveal_cards):
     print("pick cards")
 
 #Check matching cards
-def match(board, row1, column1, row2, column2, row3, column3):
+def match(board, row1, column1, row2, column2, row3, column3, row4, column4):
     print("check matching cards")
 
 #Display cards on board
