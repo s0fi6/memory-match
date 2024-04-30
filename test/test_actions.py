4183a6f72a5e760d82e9b454a612b0f38b74d610
s0fi6 <sofigp2507@gmail.com>
2024-04-30 17:55:02 -0400

    ASSERT SHUFFLE

--- test/test_actions.py
+++ test/test_actions.py
@@ -5,16 +5,17 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from memory_match.actions import shuffle_cards, flip_cards, match
 from memory_match.board import print_board, new_board
 
-def test_shuffle(): #ASSERT = please make sure that
+#ASSERT = please make sure that
+def test_shuffle(): 
   print("Testing shuffle card")
   board, flipped = new_board()
   shuffled_board = shuffle_cards(board, flipped)
   print("Shuffled board:")
-  print_board(shuffled_board)
+  print_board(shuffled_board, flipped)
   print("Expected: Shuffled board")
   print("Actual:", shuffled_board)
-  assert 
-  #Is the first card still the same? 
+  assert shuffled_board != board, flipped 
+ 
 
 def test_flip_cards():
   print("Testing flip cards")
