2c190a955bffe62336511a29a2315f9842ff2404
BernardoTorano <a01710456@tec.mx>
2024-04-29 20:20:55 -0400

    test_match, display_card

--- memory_match/memorymatch_folder/test/test_actions.py
+++ memory_match/memorymatch_folder/test/test_actions.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from actions import shuffle_cards, reveal_cards, flip_cards
+from actions import display_cards, shuffle_cards, reveal_cards, flip_cards, match, display_cards
 from board import print_board
 def test_shuffle_cards():
     
@@ -23,18 +23,18 @@ def test_reveal_cards():
     # Test case 1: Reveal a card at row 2, column 3
     print("\nTest Case 1:")
     print("Expected Output: reveal cards")
-    reveal_cards([[" ", " ", " ", " "],
-                  [" ", " ", " ", " "],
-                  [" ", " ", " ", " "],
-                  [" ", " ", " ", " "]], 2, 3)
+    reveal_cards([["DOG", "CAT", "GIRAFFE", "RACOON"],
+                    ["DOG", "CAT", "GIRAFFE", "RACOON"],
+                    ["BEE", "PANDA", "FISH", "MONKEY"],
+                    ["BEE", "PANDA", "FISH", "MONKEY"]], 2, 3)
     
     # Test case 2: Reveal a card at row 0, column 0
     print("\nTest Case 2:")
     print("Expected Output: reveal cards")
-    reveal_cards([[" ", " ", " ", " "],
-                  [" ", " ", " ", " "],
-                  [" ", " ", " ", " "],
-                  [" ", " ", " ", " "]], 0, 0)
+    reveal_cards([["DOG", "CAT", "GIRAFFE", "RACOON"],
+                    ["DOG", "CAT", "GIRAFFE", "RACOON"],
+                    ["BEE", "PANDA", "FISH", "MONKEY"],
+                    ["BEE", "PANDA", "FISH", "MONKEY"]], 0, 0)
 
 
 test_reveal_cards()
@@ -73,3 +73,37 @@ def test_pick_card():
     print_board(sample_board)
 
 test_pick_card()
+
+def test_match():
+
+    sample_board = [["DOG", "CAT", "GIRAFFE", "RACOON"],
+                    ["DOG", "CAT", "GIRAFFE", "RACOON"],
+                    ["BEE", "PANDA", "FISH", "MONKEY"],
+                    ["BEE", "PANDA", "FISH", "MONKEY"]]
+
+    print("Testing match function:")
+    
+    #Matching cards
+    print("Test 1: Matching cards")
+    match(sample_board, 0, 0, 1, 0, 0, 1)
+    
+    #Non-matching cards
+    print("Test 2: Non-matching cards")
+    match(sample_board, 0, 0, 1, 0, 0, 2)
+
+test_match()
+
+def test_display_card():
+
+    sample_board = [["DOG", "CAT", "GIRAFFE", "RACOON"],
+                    ["DOG", "CAT", "GIRAFFE", "RACOON"],
+                    ["BEE", "PANDA", "FISH", "MONKEY"],
+                    ["BEE", "PANDA", "FISH", "MONKEY"]]
+    sample_cards = ["DOG", "CAT", "GIRAFFE", "RACOON", "BEE", "PANDA", "FISH", "MONKEY"]
+
+    print("Testing display cards function:")
+    display_cards(sample_board, sample_cards)
+
+test_display_card()
+    
+       
