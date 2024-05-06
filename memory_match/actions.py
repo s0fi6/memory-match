fc0d133748dd4f258620400f529afe06c5c0dd3f
MichB1223 <165333457+MichB1223@users.noreply.github.com>
2024-05-06 19:11:07 -0400

    Actions changed

--- memory_match/actions.py
+++ memory_match/actions.py
@@ -1,26 +1,23 @@
-#Shuffle cards
-import random
-
+import random 
+# Shuffle cards
 def shuffle_cards(board, flipped):
     for sublist in board:
         random.shuffle(sublist)
 
-#Reveal cards 
-def reveal_cards(board, row, column):
-    print("reveal cards")
-
-#Flip cards
+# Reveal cards
+def reveal_cards(board, row, column, flipped):
+    print(f"Revealing card at row {row}, column {column}: {board[row][column]}")
+    flipped[row][column] = True
+# Flip cards
 def flip_cards(board, row, column):
-    print("reveal cards")
+    print(f"Flipping card at row {row}, column {column}")
 
-#Check matching cards
+# Check matching cards
 def match(board, flipped, row1, column1, row2, column2):
     card1 = board[row1][column1]
     card2 = board[row2][column2]
     if card1 == card2:
         return True
     else:
-        return False
-
-
+        return False 
 
